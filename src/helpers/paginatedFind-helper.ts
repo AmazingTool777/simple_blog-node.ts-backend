d9de12db5ef7c40141fa3ed6be63a153c4b2a0b3
Tolotra <tolotor@yahoo.fr>
2022-03-29 18:33:29 +0300

    Paginated find method helper

---
+++ src/helpers/paginatedFind-helper.ts
@@ -0,0 +1,26 @@
+import { Model, FilterQuery } from "mongoose";
+
+// Interface for the paginated results
+interface PaginatedResults<ModelAttributes> {
+    count: number;
+    pages: number;
+    rows: ModelAttributes[]
+}
+
+// Interface for the query options
+interface QueryOptions<ModelAttributes> {
+    filter: FilterQuery<ModelAttributes>;
+}
+
+// The find query paginator
+async function paginatedFind<ModelAttributes>(model: Model<ModelAttributes>, page: number, limit: number, options: QueryOptions<ModelAttributes>): Promise<PaginatedResults<ModelAttributes>> {
+    const count = await model.countDocuments(options.filter);
+    const pages = Math.ceil(count / limit);
+    return {
+        count,
+        pages,
+        rows: await model.find(options.filter).skip((page - 1) * limit)
+    }
+}
+
+export default paginatedFind;
