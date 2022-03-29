e79e7ea464ca8478975ecdf519f0dad35f37c4cb
Tolotra <tolotor@yahoo.fr>
2022-03-29 13:16:12 +0300

    Category model definition

---
+++ src/models/category-model.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+// Interface for a category model's attributes
+interface CategoryAttributes {
+    label: string,
+    posts: Types.ObjectId[]
+}
+
+// Schema for a category model
+const categoryModelSchema = new Schema<CategoryAttributes>({
+    label: {
+        type: String,
+        required: true
+    },
+    posts: [Schema.Types.ObjectId]
+});
+
+// Category model
+const CategoryModel = mongoose.model('Category', categoryModelSchema);
+
+export default CategoryModel;
+
