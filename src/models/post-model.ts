3df632bb9e3c8e0d0ef9ae342fe5a52e6f230561
Tolotra <tolotor@yahoo.fr>
2022-03-29 13:24:50 +0300

    Post model definition

---
+++ src/models/post-model.ts
@@ -0,0 +1,50 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+// Network config
+import networkConfig from "../configs/network-config";
+
+// Interface for a post model's attributes
+interface PostAttributes {
+    title: string,
+    content: string,
+    photoPath: string,
+    categories: Types.ObjectId[],
+    author: Types.ObjectId
+}
+
+// Schema for a post model
+const postModelSchema = new Schema<PostAttributes>(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        photoPath: {
+            type: String,
+            required: true
+        },
+        categories: {
+            type: [Schema.Types.ObjectId],
+            required: true
+        },
+        author: Schema.Types.ObjectId
+    },
+    {
+        toObject: { virtuals: true },
+        toJSON: { virtuals: true },
+    }
+);
+
+// Virtual full photo URL
+postModelSchema.virtual('photoURL').get(function (this: PostAttributes) {
+    return this.photoPath ? `${networkConfig.url}${this.photoPath}` : null;
+});
+
+// Post model
+const PostModel = mongoose.model('Post', postModelSchema);
+
+export default PostModel;
