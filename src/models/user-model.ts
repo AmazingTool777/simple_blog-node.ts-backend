cdde292f45e3e29a6ab4fe8cf023def466361945
Tolotra <tolotor@yahoo.fr>
2022-03-29 13:08:36 +0300

    User model definition

---
+++ src/models/user-model.ts
@@ -0,0 +1,56 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+// Network config
+import networkConfig from "../configs/network-config";
+
+// Interface for a user model's attributes
+interface UserAttributes {
+    firstName: string,
+    lastName: string,
+    gender: string,
+    email: string,
+    password: string,
+    photoPath?: string | null,
+    posts: Types.ObjectId[]
+}
+
+// Schema for a user model
+const userModelSchema = new Schema<UserAttributes>(
+    {
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        gender: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        photoPath: String
+    },
+    {
+        toObject: { virtuals: true },
+        toJSON: { virtuals: true },
+    }
+);
+
+// Virtual full photo URL
+userModelSchema.virtual('photoURL').get(function (this: UserAttributes) {
+    return this.photoPath ? `${networkConfig.url}${this.photoPath}` : null;
+});
+
+// User model
+const UserModel = mongoose.model('User', userModelSchema);
+
+export default UserModel;
