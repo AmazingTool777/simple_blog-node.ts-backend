1a19ecbb5bae6d267318bd10a1c745e7d4a0952a
Tolotra <tolotor@yahoo.fr>
2022-04-02 08:45:47 +0300

    Posts controller initial setup

---
+++ src/controllers/posts-controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+
+// App error class
+import AppError from "../@types/AppError-class";
+
+// Paginated find
+import paginatedFind from "../helpers/paginatedFind-helper";
+
+// Post model
+import PostModel from "../models/post-model";
+
+// Class for posts controller
+class PostsController {
+
+}
+
+export default PostsController;
