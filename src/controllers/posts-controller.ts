3b26a2290f0a749053c20d436a8f7bcff74f5bbe
Tolotra <tolotor@yahoo.fr>
2022-04-03 18:23:59 +0300

    Add post controller and route finished

--- src/controllers/posts-controller.ts
+++ src/controllers/posts-controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { FilterQuery } from "mongoose";
+import { FilterQuery, Document, Types } from "mongoose";
 
 // App error class
 import AppError from "../@types/AppError-class";
@@ -7,8 +7,12 @@ import AppError from "../@types/AppError-class";
 // Paginated find
 import paginatedFind from "../helpers/paginatedFind-helper";
 
-// Post model
+// Models
 import PostModel, { PostAttributes } from "../models/post-model";
+import CategoryModel, { CategoryAttributes } from "../models/category-model";
+
+// Posts photos config
+import { postsPhotoConfig } from "../configs/photos-config";
 
 // Class for posts controller
 class PostsController {
@@ -48,8 +52,40 @@ class PostsController {
     static async addPost(req: Request, res: Response, next: NextFunction) {
         try {
             const { userId } = res.locals.authUser;
-            console.log(userId);
-            res.send('Posts added!');
+            const photoFilename = req.file?.filename;
+
+            // Creating the new post document
+            const post = new PostModel({
+                title: req.body.title,
+                content: req.body.content,
+                photoPath: `${postsPhotoConfig.urlPath}/${photoFilename}`,
+                author: userId
+            });
+
+            // Checks if there are new categories that already exist
+            const checkExistingNewCategories = await Promise.allSettled((req.body.newCategories as string[]).map(label => {
+                return CategoryModel.findOne({ label: new RegExp(label, "i") });
+            }));
+            // Distinguishing the already existing categories and the non-existing ones
+            const existingCategories: Types.ObjectId[] = [];
+            const nonExistingCategories: string[] = [];
+            checkExistingNewCategories.forEach((result, i) => {
+                if (result.status === "fulfilled") {
+                    if (result.value) existingCategories.push(result.value._id);
+                    else nonExistingCategories.push(req.body.newCategories[i]);
+                }
+                else nonExistingCategories.push(req.body.newCategories[i]);
+            });
+            // Storing the non-existing categories
+            const addedCategories = await CategoryModel.insertMany(nonExistingCategories.map(label => ({ label, posts: [post._id] })));
+
+            // Setting the final categories for the post
+            post.categories = [... new Set<Types.ObjectId>([...(req.body.categories as Types.ObjectId[]), ...existingCategories]), ...addedCategories.map(({ _id }) => _id)];
+
+            // Saving the post to db
+            await post.save();
+
+            res.send(post);
         }
         catch (error) {
             next(error);
