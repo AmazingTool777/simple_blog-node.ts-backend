e8858d83011f625af5bccbba883c86afadf7818a
Tolotra <tolotor@yahoo.fr>
2022-04-04 21:15:49 +0300

    Changing the photos config based on the move of static folder

--- src/configs/photos-config.ts
+++ src/configs/photos-config.ts
@@ -8,13 +8,13 @@ interface PhotoConfig {
 
 // Photo config for the users'photos
 const usersPhotoConfig: PhotoConfig = {
-    storagePath: path.join(__dirname, "../", "static", "users"),
+    storagePath: path.join(__dirname, "../", "../", "static", "users"),
     urlPath: "/public/users"
 }
 
 // Photo config for the posts'photos
 const postsPhotoConfig: PhotoConfig = {
-    storagePath: path.join(__dirname, "../", "static", "posts"),
+    storagePath: path.join(__dirname, "../", "../", "static", "posts"),
     urlPath: "/public/posts"
 }
 
